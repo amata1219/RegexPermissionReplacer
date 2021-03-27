6ef21f9c0734b87e348f0578e88b36dec36d524c
amata1219 <suzukiamata@gmail.com>
2021-03-28 00:11:53 +0900

    LuckPermsのブリッジのgetterを追加

--- src/amata1219/regex/permission/replacer/RegexPermissionReplacer.java
+++ src/amata1219/regex/permission/replacer/RegexPermissionReplacer.java
@@ -1,14 +1,20 @@
 package amata1219.regex.permission.replacer;
 
 import amata1219.regex.permission.replacer.bridge.LuckPermsBridge;
+import amata1219.regex.permission.replacer.operation.record.OperationRecord;
 import net.luckperms.api.LuckPerms;
 import org.bukkit.event.HandlerList;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegexPermissionReplacer extends JavaPlugin {
 
     private static RegexPermissionReplacer instance;
     private LuckPermsBridge luckPermsBridge;
+
+    private final Map<Long, OperationRecord> operationRecords = new HashMap<>();
 
     @Override
     public void onEnable() {
@@ -23,6 +29,10 @@ public class RegexPermissionReplacer extends JavaPlugin {
 
     public static RegexPermissionReplacer instance() {
         return instance;
+    }
+
+    public LuckPermsBridge luckPermsBridge() {
+        return luckPermsBridge;
     }
 
 }
