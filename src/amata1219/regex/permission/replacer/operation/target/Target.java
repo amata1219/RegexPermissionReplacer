2130a34fa7e4e454dfff30964c9297b70c68c159
amata1219 <suzukiamata@gmail.com>
2021-03-27 21:55:49 +0900

    ターゲットを表現するクラスを追加

---
+++ src/amata1219/regex/permission/replacer/operation/target/Target.java
@@ -0,0 +1,33 @@
+package amata1219.regex.permission.replacer.operation.target;
+
+import com.google.common.collect.ImmutableSet;
+
+import java.util.UUID;
+
+public interface Target {
+
+    class All implements Target {
+
+    }
+
+    class Group implements Target {
+
+        public final String groupName;
+
+        public Group(String groupName) {
+            this.groupName = groupName;
+        }
+
+    }
+
+    class Players implements Target {
+
+        public final ImmutableSet<UUID> playersUniqueIds;
+
+        public Players(ImmutableSet<UUID> playersUniqueIds) {
+            this.playersUniqueIds = playersUniqueIds;
+        }
+
+    }
+
+}
