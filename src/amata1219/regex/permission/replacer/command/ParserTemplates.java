f9843e65c38baf232e03e03e940a55a9db49fe02
amata1219 <suzukiamata@gmail.com>
2021-03-28 15:10:41 +0900

    操作IDのパーサを追加

--- src/amata1219/regex/permission/replacer/command/ParserTemplates.java
+++ src/amata1219/regex/permission/replacer/command/ParserTemplates.java
@@ -1,6 +1,8 @@
 package amata1219.regex.permission.replacer.command;
 
+import amata1219.regex.permission.replacer.OperationId;
 import amata1219.regex.permission.replacer.RegexPermissionReplacer;
+import amata1219.regex.permission.replacer.bryionake.constant.Parsers;
 import amata1219.regex.permission.replacer.bryionake.dsl.parser.FailableParser;
 import net.luckperms.api.model.group.Group;
 import net.luckperms.api.model.group.GroupManager;
@@ -14,5 +16,7 @@ public class ParserTemplates {
         GroupManager manager = RegexPermissionReplacer.instance().luckPermsBridge().luckPerms.getGroupManager();
         return manager.isLoaded(arg) ? success(manager.getGroup(arg)) : failure(ChatColor.RED + "指定されたグループは存在しません。");
     };
+
+    public static final FailableParser<OperationId> operationId = Parsers.i64.append(value -> OperationId.valueIsIssuedOperationId(value) ? success(new OperationId(value)) : failure(ChatColor.RED + "指定された操作IDは存在しません。"));
 
 }
