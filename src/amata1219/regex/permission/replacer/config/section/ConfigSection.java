17ee0aa758c3ae19de90a5c0b3089dc755cd616b
amata1219 <suzukiamata@gmail.com>
2021-03-28 15:33:25 +0900

    コンフィグのセクションを表すクラスを追加

---
+++ src/amata1219/regex/permission/replacer/config/section/ConfigSection.java
@@ -0,0 +1,20 @@
+package amata1219.regex.permission.replacer.config.section;
+
+import amata1219.regex.permission.replacer.config.Config;
+import org.bukkit.configuration.ConfigurationSection;
+
+public abstract class ConfigSection {
+
+    private final Config config;
+    private final String sectionName;
+
+    public ConfigSection(Config config, String sectionName) {
+        this.config = config;
+        this.sectionName = sectionName;
+    }
+
+    protected ConfigurationSection section() {
+        return config.config().getConfigurationSection(sectionName);
+    }
+
+}
