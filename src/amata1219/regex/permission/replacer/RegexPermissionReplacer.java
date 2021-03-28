f4c530f43bd76903555aa561a0d71401fa59bf24
amata1219 <suzukiamata@gmail.com>
2021-03-28 16:26:14 +0900

    メインコンフィグのgetterを追加

--- src/amata1219/regex/permission/replacer/RegexPermissionReplacer.java
+++ src/amata1219/regex/permission/replacer/RegexPermissionReplacer.java
@@ -2,6 +2,7 @@ package amata1219.regex.permission.replacer;
 
 import amata1219.regex.permission.replacer.bridge.LuckPermsBridge;
 import amata1219.regex.permission.replacer.command.MainCommand;
+import amata1219.regex.permission.replacer.config.MainConfig;
 import amata1219.regex.permission.replacer.operation.record.OperationRecord;
 import net.luckperms.api.LuckPerms;
 import org.bukkit.event.HandlerList;
@@ -14,13 +15,15 @@ public class RegexPermissionReplacer extends JavaPlugin {
     private static RegexPermissionReplacer instance;
     private LuckPermsBridge luckPermsBridge;
 
+    private MainConfig config;
     private final TreeMap<OperationId, OperationRecord> operationRecords = new TreeMap<>();
 
     @Override
     public void onEnable() {
         instance = this;
         luckPermsBridge = new LuckPermsBridge((LuckPerms) getServer().getPluginManager().getPlugin("LuckPerms"));
-        getCommand("rpr").setExecutor(new MainCommand(operationRecords));
+        config = new MainConfig();
+        getCommand("rpr").setExecutor(new MainCommand(config, operationRecords));
     }
 
     @Override
